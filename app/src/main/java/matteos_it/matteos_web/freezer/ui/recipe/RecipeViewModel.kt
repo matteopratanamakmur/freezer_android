82a11c0cc0f02664ef29830243e96795b73bd8ad
matteopratanamakmur <matteopratanamakmur.Main@gmail.com>
2021-01-05 16:32:23 +0900

    Initial commit

---
+++ app/src/main/java/matteos_it/matteos_web/freezer/ui/recipe/RecipeViewModel.kt
@@ -0,0 +1,13 @@
+package matteos_it.matteos_web.freezer.ui.recipe
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RecipeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is recipe Fragment"
+    }
+    val text: LiveData<String> = _text
+}
